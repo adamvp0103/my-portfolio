923655e97040bb1ad33263fc720d2cc2bec433c8
Adam Parks <adamvp0103@gmail.com>
2025-05-14 15:39:32 -0400

    Style contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,5 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import sharedStyles from '../../shared/shared.module.css';
 import styles from './Contact.module.css';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Contact() {
   return (
@@ -10,17 +13,25 @@ export default function Contact() {
 
       <div className={sharedStyles.page}>
         {/* TODO: Replace lorem with thank you message */}
-        <p>
+        <p className={styles.message}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
           saepe!
         </p>
         {/* TODO: Implement links */}
-        <a>LinkedIn</a>
-        <br />
-        <a>GitHub</a>
-        <br />
-        <a href="mailto:adamvp0103@gmail.com?subject=Employment%20Opportunity">
-          Email
+        <a className={styles.link} href="#">
+          <FontAwesomeIcon className={sharedStyles.icon} icon={faLinkedin} />
+          &nbsp;&nbsp;LinkedIn
+        </a>
+        <a className={styles.link} href="#">
+          <FontAwesomeIcon className={sharedStyles.icon} icon={faGithub} />
+          &nbsp;&nbsp;GitHub
+        </a>
+        <a
+          className={styles.link}
+          href="mailto:adamvp0103@gmail.com?subject=Employment%20Opportunity"
+        >
+          <FontAwesomeIcon className={sharedStyles.icon} icon={faEnvelope} />
+          &nbsp;&nbsp;Email
         </a>
       </div>
     </div>
