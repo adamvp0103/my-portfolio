97bd662d1b2e5f5d67e579f6b1e75559394dbfda
Adam Parks <adamvp0103@gmail.com>
2025-05-27 16:06:48 -0400

    Fix routes

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,15 +7,15 @@ import { faCode, faEnvelope, faHouse } from '@fortawesome/free-solid-svg-icons';
 export default function Nav() {
   return (
     <nav className={styles.nav} onClick={() => window.scrollTo({ top: 0 })}>
-      <Link className={styles.link} to="/">
+      <Link className={styles.link} to="/my-portfolio/">
         <FontAwesomeIcon className={sharedStyles.icon} icon={faHouse} />
         <div className={styles.text}>&nbsp;&nbsp;Home</div>
       </Link>
-      <Link className={styles.link} to="/projects">
+      <Link className={styles.link} to="/my-portfolio/projects">
         <FontAwesomeIcon className={sharedStyles.icon} icon={faCode} />
         <div className={styles.text}>&nbsp;&nbsp;Projects</div>
       </Link>
-      <Link className={styles.link} to="/contact">
+      <Link className={styles.link} to="/my-portfolio/contact">
         <FontAwesomeIcon className={sharedStyles.icon} icon={faEnvelope} />
         <div className={styles.text}>&nbsp;&nbsp;Contact</div>
       </Link>
